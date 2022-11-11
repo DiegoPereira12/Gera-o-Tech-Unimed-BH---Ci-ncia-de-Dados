3e38467ff83e761ba6075f44f8db69491eb1dfe1
unknown <diegoo.junior55@gmail.com>
2022-11-10 23:36:00 -0300

    update files 10112022

---
+++ Desafio Intermediário Py/desafio_intermediario_01.py
@@ -0,0 +1,6 @@
+letra = input()
+
+#lista = [chr(i) for i in range(ord('A')-1,ord('Z')+1)]
+lista = ['null','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+print (lista.index(letra))
+
